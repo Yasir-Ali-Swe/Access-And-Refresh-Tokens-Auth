5a1adc1d17d102d0f448373fe104930f8186fcb7
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-02-07 01:04:09 +0500

    Backend

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import connectDB from './config/db.connection.js';
+import { PORT } from './config/env.js';
+import authRoutes from './routes/auth.route.js';
+
+const app = express();
+app.use(cors(
+    {
+        origin: true,
+        methods: ['GET', 'POST', 'PUT', 'DELETE',"PUT","PATCH"],
+        credentials: true,
+    }
+));
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.listen(PORT, async () => {
+    console.log(`Server is running on port ${PORT}`);
+    await connectDB();
+});
+
+app.use('/api/auth', authRoutes);
+
