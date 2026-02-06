0deb2d40c4a26d405025cfea9cca84d669b60d90
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-02-07 01:09:01 +0500

    Add axios helper

---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+import store from "../store/store";
+import { setAccessToken, logout } from "../features/auth/authSlice";
+
+const URL = "http://localhost:3000/api";
+
+const api = axios.create({
+  baseURL: URL,
+  withCredentials: true,
+});
+
+api.interceptors.request.use((config) => {
+  const token = store.getState().auth.accessToken;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        const res = await axios.post(
+          "http://localhost:3000/api/auth/refresh",
+          {},
+          { withCredentials: true },
+        );
+        store.dispatch(setAccessToken(res.data.accessToken));
+        originalRequest.headers.Authorization = `Bearer ${res.data.accessToken}`;
+        return api(originalRequest);
+      } catch (err) {
+        store.dispatch(logout());
+        window.location.href = "/login";
+      }
+    }
+    return Promise.reject(error);
+  },
+);
+
+export default api;
