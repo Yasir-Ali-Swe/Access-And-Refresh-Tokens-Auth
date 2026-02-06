beba7464def2d1e050cf0bfa6e251aaddf6ca8d4
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-02-07 01:08:48 +0500

    Add auth API feature

---
+++ frontend/src/features/auth/authApi.js
@@ -0,0 +1,26 @@
+import api from "@/lib/axios.js";
+
+export const register = async (data) => {
+  const response = await api.post("/auth/register", data);
+  return response.data;
+};
+
+export const login = async (data) => {
+  const response = await api.post("/auth/login", data);
+  return response.data;
+};
+
+export const verifyEmail = async (token) => {
+  const response = await api.post(`/auth/verify-email/${token}`);
+  return response.data;
+};
+
+export const logout = async () => {
+  const response = await api.post("/auth/logout");
+  return response.data;
+};
+
+export const getMe = async () => {
+  const response = await api.get("/auth/me");
+  return response.data;
+};
