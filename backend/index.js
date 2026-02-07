fc52e09d7875fe8d823d81310e4183a54aa61317
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-02-07 14:18:09 +0500

    Update backend server entry

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,24 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import connectDB from './config/db.connection.js';
-import { PORT } from './config/env.js';
-import authRoutes from './routes/auth.route.js';
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { connectDB } from "./config/connect.db.js";
+import { PORT } from "./config/env.js";
+import authRoutes from "./routes/auth.route.js";
 
 const app = express();
-app.use(cors(
-    {
-        origin: true,
-        methods: ['GET', 'POST', 'PUT', 'DELETE',"PUT","PATCH"],
-        credentials: true,
-    }
-));
+app.use(
+  cors({
+    origin: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PUT", "PATCH"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(cookieParser());
 
-
 app.listen(PORT, async () => {
-    console.log(`Server is running on port ${PORT}`);
-    await connectDB();
+  console.log(`Server is running on port ${PORT}`);
+  await connectDB();
 });
 
-app.use('/api/auth', authRoutes);
-
+app.use("/api/auth", authRoutes);
